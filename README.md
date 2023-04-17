405fbcf86107c907c7dd8320cae30072a72517ad
R <ramz9_97@hotmail.com>
2023-04-17 23:29:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## BlogTonic backend
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
