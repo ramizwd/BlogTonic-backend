87830caa133ad53d0f0a40f0291c72793345a6ed
R <ramz9_97@hotmail.com>
2023-04-26 23:49:40 +0300

    test cases

--- test/userFunctions.ts
+++ test/userFunctions.ts
@@ -3,6 +3,8 @@ import request from 'supertest';
 import expect from 'expect';
 import {UserTest} from '../src/interfaces/User';
 import LoginMessageResponse from '../src/interfaces/LoginMessageResponse';
+import randomstring from 'randomstring';
+import ErrorResponse from '../src/interfaces/ErrorResponse';
 
 /* test for graphql query
 mutation Register($user: UserInput!) {
@@ -115,6 +117,60 @@ const login = (
   });
 };
 
+const loginBrute = (
+  url: string | Function,
+  user: UserTest
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    request(url)
+      .post('/graphql')
+      .set('Content-type', 'application/json')
+      .send({
+        query: `mutation Login($credentials: Credentials!) {
+                        login(credentials: $credentials) {
+                            message
+                            user {
+                                id
+                                email
+                                username
+                            }
+                            token
+                        }
+                    }`,
+        variables: {
+          credentials: {
+            username: user.email,
+            password: user.password,
+          },
+        },
+      })
+      .expect(200, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (
+            response.body.errors?.[0]?.message ===
+            'You have been blocked from logging in'
+          ) {
+            console.log('brute force blocked', response.body.errors[0].message);
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      });
+  });
+};
+
+/* test for graphql query
+query Users {
+  users {
+    email
+    id
+    username
+  }
+}
+*/
 const getUsers = (url: string | Function): Promise<UserTest> => {
   return new Promise((resolve, reject) => {
     request(url)
@@ -180,4 +236,109 @@ const getUserById = (url: string | Function, id: string): Promise<UserTest> => {
   });
 };
 
-export {createUser, login, getUsers, getUserById};
+/* test for graphql query
+mutation UpdateUser($user: UserModify!) {
+  updateUser(user: $user) {
+    message
+    user {
+      email
+      id
+      username
+    }
+  }
+}
+*/
+const updateUser = (url: string | Function, token: string) => {
+  return new Promise((resolve, reject) => {
+    const newUser = 'newUser' + randomstring.generate(5);
+    request(url)
+      .post('/graphql')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', 'Bearer ' + token)
+      .send({
+        query: `mutation UpdateUser($user: UserModify!) {
+                    updateUser(user: $user) {
+                        message
+                        user {
+                            email
+                            id
+                            username
+                        }
+                    }
+                }`,
+        variables: {
+          user: {
+            username: newUser,
+          },
+        },
+      })
+      .expect(200, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          const userData = response.body.data.updateUser;
+          expect(userData).toHaveProperty('message');
+          expect(userData).toHaveProperty('user');
+          expect(userData.user).toHaveProperty('id');
+          expect(userData.user.username).toBe(newUser);
+          resolve(response.body.data.updateUser);
+        }
+      });
+  });
+};
+
+/* test for graphql query
+mutation DeleteUser {
+  deleteUser {
+    message
+    user {
+      email
+      id
+      username
+    }
+  }
+}
+*/
+const deleteUser = (
+  url: string | Function,
+  token: string
+): Promise<ErrorResponse> => {
+  return new Promise((resolve, reject) => {
+    request(url)
+      .post('/graphql')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', 'Bearer ' + token)
+      .send({
+        query: `mutation DeleteUser {
+                    deleteUser {
+                        message
+                        user {
+                            email
+                            id
+                            username
+                        }
+                    }
+                }`,
+      })
+      .expect(200, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          const userData = response.body.data.deleteUser;
+          expect(userData).toHaveProperty('message');
+          expect(userData).toHaveProperty('user');
+          resolve(response.body.data.deleteUser);
+        }
+      });
+  });
+};
+
+export {
+  createUser,
+  login,
+  loginBrute,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
