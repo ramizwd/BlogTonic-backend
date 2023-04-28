9c6de5d8bf9fa434ca738bdf00ac18f57a87ace2
R <ramz9_97@hotmail.com>
2023-04-28 17:39:25 +0300

    organized test

--- test/api.test.ts
+++ test/api.test.ts
@@ -11,7 +11,6 @@ import {
   getUserById,
   getUsers,
   login,
-  loginBrute,
   updateUser,
 } from './userFunctions';
 import {PostTest} from '../src/interfaces/Post';
@@ -29,17 +28,8 @@ describe('GraphQL API tests', () => {
     await mongoose.connect(DATABASE_URL);
   });
 
-  afterAll(async () => {
-    await mongoose.disconnect();
-    await mongoose.connection.close();
-  });
-
-  it('should respond with not found message', async () => {
-    await getNotFound(app);
-  });
-
-  let userData: LoginMessageResponse;
-  let userData2: LoginMessageResponse;
+  let loggedInUser: LoginMessageResponse;
+  let loggedInUser2: LoginMessageResponse;
 
   const testUser: UserTest = {
     username: 'TestUser' + randomstring.generate(7),
@@ -53,103 +43,95 @@ describe('GraphQL API tests', () => {
     password: 'testpassword1234!',
   };
 
-  // create first user
-  it('should create a new user', async () => {
-    await createUser(app, testUser);
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
   });
 
-  // create second user to try to modify someone else's blog and userdata
-  it('should create second user', async () => {
-    await createUser(app, testUser2);
+  it('should respond with not found message', async () => {
+    await getNotFound(app);
   });
 
-  // test login
-  it('should login user', async () => {
-    userData = await login(app, testUser);
+  describe('User tests', () => {
+    // create first user
+    it('should create a new user', async () => {
+      await createUser(app, testUser);
+    });
+
+    // create second user to try to modify someone else's blog and user data
+    it('should create second user', async () => {
+      await createUser(app, testUser2);
+    });
+
+    // test login
+    it('should login user', async () => {
+      loggedInUser = await login(app, testUser);
+    });
+
+    // test login with second user
+    it('should login second user', async () => {
+      loggedInUser2 = await login(app, testUser2);
+    });
+
+    // test get all users
+    it('should return an array of users', async () => {
+      await getUsers(app);
+    });
+
+    // test get single user
+    it('should return single user', async () => {
+      await getUserById(app, loggedInUser.user.id!);
+    });
+
+    // test update user
+    it('should update user', async () => {
+      await updateUser(app, loggedInUser.token!);
+    });
+
+    // test delete user based on token
+    it('should delete current user', async () => {
+      await deleteUser(app, loggedInUser.token!);
+    });
+
+    // make sure token has role (so that we can test if user is admin or not)
+    it('should have isAdmin property in token data', async () => {
+      const dataFromToken = jwt.verify(
+        loggedInUser.token!,
+        process.env.JWT_SECRET as string
+      );
+      expect(dataFromToken).toHaveProperty('isAdmin');
+    });
   });
 
-  // test login with second user
-  it('should login second user', async () => {
-    userData2 = await login(app, testUser2);
+  describe('Post tests', () => {
+    let postID1: string;
+
+    const testPost: PostTest = {
+      title: 'test title',
+      content: 'test content',
+    };
+
+    // test create post
+    it('should create a new post', async () => {
+      await createUser(app, testUser);
+      loggedInUser = await login(app, testUser);
+      const post = await createPost(app, testPost, loggedInUser.token!);
+      postID1 = post.id!;
+    });
+
+    // test get all posts
+    it('should return an array of posts', async () => {
+      await getPosts(app);
+    });
+
+    // test get post by id
+    it('should return single post', async () => {
+      await getPostById(app, postID1);
+    });
+
+    // test get posts by author id
+    it('should return posts by author id', async () => {
+      await getPostsByAuthorId(app, loggedInUser.user.id!);
+    });
   });
-
-  // test get all users
-  it('should return an array of users', async () => {
-    await getUsers(app);
-  });
-
-  // test get single user
-  it('should return single user', async () => {
-    await getUserById(app, userData.user.id!);
-  });
-
-  // make sure token has role (so that we can test if user is admin or not)
-  it('token should have role', async () => {
-    const dataFromToken = jwt.verify(
-      userData.token!,
-      process.env.JWT_SECRET as string
-    );
-    expect(dataFromToken).toHaveProperty('isAdmin');
-  });
-
-  // test update user
-  it('should update user', async () => {
-    await updateUser(app, userData.token!);
-  });
-
-  // test delete user based on token
-  it('should delete current user', async () => {
-    await deleteUser(app, userData.token!);
-  });
-
-  const testPost: PostTest = {
-    title: 'test title',
-    content: 'test content',
-  };
-
-  // test create post
-  let postID1: string;
-  it('should create a new post', async () => {
-    await createUser(app, testUser);
-    userData = await login(app, testUser);
-    const post = await createPost(app, testPost, userData.token!);
-    postID1 = post.id!;
-  });
-
-  // test get all posts
-  it('should return an array of posts', async () => {
-    await getPosts(app);
-  });
-
-  // test get post by id
-  it('should return single post', async () => {
-    await getPostById(app, postID1);
-  });
-
-  // test get posts by author id
-  it('should return posts by author id', async () => {
-    await getPostsByAuthorId(app, userData.user.id!);
-  });
-
-  //   test brute force protection
-  //   test('Brute force attack simulation', async () => {
-  //     const maxAttempts = 20;
-  //     const mockUser: UserTest = {
-  //       username: 'TestUser' + randomstring.generate(7),
-  //       email: randomstring.generate(9).toLowerCase() + '@user.com',
-  //       password: 'wrongpassword123!',
-  //     };
-
-  //     try {
-  //       for (let i = 0; i < maxAttempts; i++) {
-  //         const result = await loginBrute(app, mockUser);
-  //         if (result) throw new Error('Brute force attack unsuccessful');
-  //       }
-
-  //       throw new Error('Brute force attack succeeded');
-  //     } catch (error) {
-  //       console.log(error);
-  //       expect((error as Error).message).toBe('Brute force attack unsuccessful');
-  //     }
-  //   }, 15000);
 });
